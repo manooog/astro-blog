d488629f851d976adefc59dd195e133fd8217288
ruanxihao <ruanxihao@xiaomi.com>
2022-10-11 18:07:47 +0800

    fix: 生产环境使用 mmysql 配置，测试环境还是用 sqlite

--- packages/server/config/database.js
+++ packages/server/config/database.js
@@ -1,11 +1,32 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT", "sqlite");
+  let mysqlConnectOptions = {};
+  if (client === "mysql") {
+    // mysql 数据库使用环境变量
+    mysqlConnectOptions = {
+      host: env("DATABASE_HOST", "0.0.0.0"),
+      port: env("DATABASE_PORT", "3306"),
+      database: env("DATABASE_NAME", "blog-strapi"),
+      user: env("DATABASE_USERNAME", "user"),
+      password: env("DATABASE_PASSWORD", "password"),
+      timezone: env("TZ", "Asia/Shanghai"),
+    };
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client,
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+        ...mysqlConnectOptions,
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
